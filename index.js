fa29220547be8d567fe645ff73aaab49b365695c
Ball Man <joshua.christensen13@gmail.com>
2023-04-27 21:48:34 -0600

    made a html and javascript file, linked files and coded the jumbotron.

---
+++ index.js
@@ -0,0 +1,11 @@
+var coins = ["/assets/images/Heads.jpg", "/assets/images/Tails.jpg"];
+
+// var flip = document.getElementById("flip").addEventListener("click", function(){
+//     alert("clicked");
+// });
+
+$(document).ready(function(){
+    $("#flip").click(function(){
+        alert("clicked");
+    });
+});
