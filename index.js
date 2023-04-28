4081551527739c9ce7f53b13ecc3961d255b4b07
Ball Man <joshua.christensen13@gmail.com>
2023-04-27 23:17:30 -0600

    added buttons to click and count the number of times each coin is clicked through an alert then chained an image to the h1 with the id of coin-spot. Next will work on using one button to randomly pick on side and display that image.

--- index.js
+++ index.js
@@ -3,9 +3,36 @@ var coins = ["/assets/images/Heads.jpg", "/assets/images/Tails.jpg"];
 // var flip = document.getElementById("flip").addEventListener("click", function(){
 //     alert("clicked");
 // });
+var headsCount = 0;
+var tailsCount = 0;
 
 $(document).ready(function(){
-    $("#flip").click(function(){
-        alert("clicked");
+    $("#heads").click(function(){
+        headsCount++;
+        alert("Heads " + headsCount);
+        var imgDiv = $("<img src ='/assets/images/Heads.jpg'>")
+        $("#coin-spot").html(imgDiv);
+    });
+
+        $("#tails").click(function(){
+            tailsCount++;
+            alert("Tails " + tailsCount);
+            var imgDiv = $("<img src ='/assets/images/Tails.jpg'>")
+            $("#coin-spot").html(imgDiv);
+    
+        // var choice = Math.floor(Math.random()* coins.length);
+        // console.log(choice);
+        // var newDiv = $("<div>");
+        // newDiv.text(choice);
+        // $("#coin-spot").html(newDiv);
+
+    //    for(var i = 0; i < coins.length; i++){
+    //     var choice = Math.floor(Math.random()* coins.length);
+    //     console.log(choice);
+    //     var newDiv = $("<div>");
+    //     newDiv.text(choice);
+    //     $("#coin-spot").html(newDiv);
+    //    }
+
     });
 });
