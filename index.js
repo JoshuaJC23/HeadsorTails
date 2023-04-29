e4f675647086b0efd5b1e1f9c1912ba4e7607957
Ball Man <joshua.christensen13@gmail.com>
2023-04-28 22:29:55 -0600

    added html with tails and heads then hid them until button is clicked, and has a counter so you can see how many times it comes up heads or tails and you can tell if it is the same side since the picture doesn't change.

--- index.js
+++ index.js
@@ -3,16 +3,26 @@ var headsCount = 0;
 var tailsCount = 0;
 
 $(document).ready(function(){
+    $("#heads").hide();
+    $("#tails").hide();
+    
     $("#coin").click(function(){
         
         var random = Math.floor(Math.random() * 2);
         console.log(random);
-
+        $("#heads").show();
+        $("#tails").show();
+        
         if(random == 0){
-            $("#coin-spot").html($("<img src = '/assets/images/Heads.jpg'>"));
+            $("#coin-spot").html($("<img src = '/assets/images/Heads.jpg' width = 300 height = 300>"));
+            headsCount++;
+            $("#headsCount").text(headsCount);
         }
+        
         if(random == 1) {
-            $("#coin-spot").html($("<img src = '/assets/images/Tails.jpg'>"));
+            $("#coin-spot").html($("<img src = '/assets/images/Tails.jpg' width=300 height=300>"));
+            tailsCount++;
+            $("#tailsCount").text(tailsCount);
         };
 
     });
